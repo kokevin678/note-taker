2a4d4ccb56edd65ed2c2e08666f042dd7546ba75
Chengyu Ko <kokevin678@gmail.com>
2020-01-14 19:26:38 -0800

    got index.html and note.html routing working

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,16 @@
+// Dependencies
+var path = require("path");
+
+// Routing
+module.exports = function(app) {
+
+    // HTML GET Request
+    app.get("/notes", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+
+  // If no matching route is found default to home
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
+};
